5a2414585644057d8563767ce329ece2cc7dd0a6
lapin <delcoignevincent@mailbox.org>
2023-08-06 08:16:40 +0200

    Set duration to correct date

--- TimesheetFs/Excel.fs
+++ TimesheetFs/Excel.fs
@@ -1,5 +1,6 @@
 ﻿module Excel
 
+open System
 open System.IO
 open FsExcel
 open Types
@@ -7,13 +8,13 @@ open Types
 let genDates (startDate: System.DateTime) =
     let endDate = startDate.AddMonths(1).AddDays(-1)
 
-    Seq.unfold
+    startDate |> Seq.unfold
         (fun date ->
             if date <= endDate then
                 Some(date, date.AddDays(1.0))
             else
                 None)
-        startDate
+        |> Seq.map (DateOnly.FromDateTime)
 
 let generateExcel (date: System.DateTime) (timeEntries: MyTimeEntry2 list) =
     let savePath = "/home/lapin"
@@ -21,15 +22,17 @@ let generateExcel (date: System.DateTime) (timeEntries: MyTimeEntry2 list) =
     [
       Go(Indent 2)
       for date in genDates date do
-          Cell [ DateTime date ]
+          Cell [ DateTime (date.ToDateTime TimeOnly.MinValue) ]
       Go NewRow
       Go(Indent 1)
       for prjName, list in (timeEntries |> List.groupBy (fun te -> te.ProjectName)) do
           Cell [ String prjName ]
 
-          for item in list do
-              Cell [ TimeSpan item.Duration ]
-
+          for date in genDates date do
+              match list |> List.tryFind (fun te -> te.Date = date) with
+              | None -> Cell [ String "" ]
+              | Some item -> Cell [ TimeSpan item.Duration ]
+              
           Go NewRow
           SizeAll (ColWidth 15)
     ]
