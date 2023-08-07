49293410191525d275d73bd555378035e84a664e
Vincent DELCOIGNE <vdelcoigne@actiris.be>
2023-08-07 17:11:21 +0200

    Parameter for date

--- TimesheetFs/Program.fs
+++ TimesheetFs/Program.fs
@@ -1,40 +1,39 @@
 ﻿open System
+open System.Globalization
 open Toggl.Api
+open FSharp.SystemCommandLine
 open Timesheet
 open Excel
-open FSharp.SystemCommandLine
-open System.IO
 
 let client = TogglClient("77775ba928442e3ea39bcb4258a52710")
-
 let getTimeEntries = getTimeEntries client
 
 // TODO:
-// take first day of last month by default.
-// Verbose param to debug print ?
+// Appsettings for Api key
 // Refacto ts generation.
+// Verbose param to debug print ?
 // Spectre.Console
-let date = DateTime(2023, 7, 1)
 
-let generate(outputDirMaybe: DirectoryInfo option) =
-    let outputDir = defaultArg outputDirMaybe (DirectoryInfo @"C:\temp")
-    printfn $"Generating Timesheet in {outputDir} ..."
 
-    let timeEntries = getTimeEntries date
-    timeEntries |> generateExcel "C:\\temp" date |> openFile
+let generate(dateMaybe: DateTime option, outputDirMaybe: string option) =
+    let outputDir = defaultArg outputDirMaybe @"C:\temp"
+    let date = defaultArg dateMaybe (DateTime.Today.AddMonths(-1))
+    let startDate = DateTime(date.Year, date.Month, 1)
+
+    printfn $"""Generating Timesheet for {date.ToString("MMMM", CultureInfo.InvariantCulture)} in {outputDir} ..."""
+
+    getTimeEntries date |> generateExcel outputDir startDate |> openFile
 
 let outputDirMaybe =
-    Input.OptionMaybe<DirectoryInfo>([ "--output"; "-o" ], "The output directory")
+    Input.OptionMaybe<string>([ "--output"; "-o" ], "The output directory")
+
+let dateMaybe =
+    Input.OptionMaybe<DateTime>([ "--date"; "-d" ], "The timesheet date")
 
 [<EntryPoint>]
 let main argv =
     rootCommand argv {
-        description "Generate an Excel Timesheet"
-        inputs outputDirMaybe
+        description "Generates an Actiris Timesheet"
+        inputs (dateMaybe, outputDirMaybe)
         setHandler generate
     }
-
-// timeEntries
-// |> List.iter (fun te ->
-//     printfn "%A" te.Date
-//     printfn "%s : %A" te.ProjectName te.Duration)
