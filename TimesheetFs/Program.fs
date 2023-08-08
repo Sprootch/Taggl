df84bc524daf5a9bd246a8a94e02f9d06f04a9be
Vincent DELCOIGNE <vdelcoigne@actiris.be>
2023-08-08 11:39:56 +0200

    Add api key in appsettings.json

--- TimesheetFs/Program.fs
+++ TimesheetFs/Program.fs
@@ -1,39 +1,46 @@
 ﻿open System
 open System.Globalization
+open System.IO
+open Microsoft.Extensions.Configuration
 open Toggl.Api
 open FSharp.SystemCommandLine
 open Timesheet
 open Excel
 
-let client = TogglClient("77775ba928442e3ea39bcb4258a52710")
+let settings =
+    ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", false)
+        .Build()
+        
+let client = TogglClient(settings["Toggl:ApiKey"])
 let getTimeEntries = getTimeEntries client
 
 // TODO:
-// Appsettings for Api key
+// have an Excel SUM
 // Refacto ts generation.
 // Verbose param to debug print ?
 // Spectre.Console
-
 
 let generate(dateMaybe: DateTime option, outputDirMaybe: string option) =
     let outputDir = defaultArg outputDirMaybe @"C:\temp"
     let date = defaultArg dateMaybe (DateTime.Today.AddMonths(-1))
     let startDate = DateTime(date.Year, date.Month, 1)
-
+    
     printfn $"""Generating Timesheet for {date.ToString("MMMM", CultureInfo.InvariantCulture)} in {outputDir} ..."""
-
+    
     getTimeEntries date |> generateExcel outputDir startDate |> openFile
 
-let outputDirMaybe =
-    Input.OptionMaybe<string>([ "--output"; "-o" ], "The output directory")
-
-let dateMaybe =
-    Input.OptionMaybe<DateTime>([ "--date"; "-d" ], "The timesheet date")
 
 [<EntryPoint>]
 let main argv =
     rootCommand argv {
         description "Generates an Actiris Timesheet"
-        inputs (dateMaybe, outputDirMaybe)
+
+        inputs (
+            Input.OptionMaybe<DateTime>([ "--date"; "-d" ], "The timesheet date"),
+            Input.OptionMaybe<string>([ "--output"; "-o" ], "The output directory")
+        )
+
         setHandler generate
     }
