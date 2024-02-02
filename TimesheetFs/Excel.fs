3c6c155be71030de413f7664dac12a5325f365d8
Vincent DELCOIGNE <vdelcoigne@actiris.be>
2024-02-02 16:08:03 +0100

    add projects dynamically

--- TimesheetFs/Excel.fs
+++ TimesheetFs/Excel.fs
@@ -5,9 +5,6 @@ open System.IO
 open Types
 open Common
 
-// todo : add dynamically the projects.
-// gérer autre
-
 let getProjectRows(range: ExcelRange) =
     range
     |> Seq.map (fun cell -> (cell.Value |> string, cell.Start.Row))
@@ -20,38 +17,47 @@ let findProjectCode (package: ExcelPackage) (name: string) =
     range
     |> Seq.tryFind (fun cell -> cell.Value = name)
     |> Option.map (fun cell -> projectsSheet.Cells[cell.Start.Row, 1].Value |> string)
+    |> Option.defaultValue "AUTRE"
 
-let generateExcel path date timeEntries =
-    let package = new ExcelPackage("Timesheet-Template-v10.xlsx")
-    package.Workbook.Worksheets["Configuration"].Cells["D13"].Value <- date
-
+let setupProjects timeEntries (package: ExcelPackage) =
     let prestations = package.Workbook.Worksheets["Prestations"]
-
-    let days = date |> generateDaysOfMonth |> Seq.indexed
-
-    let mutable row = 7
 
     let projectCodes =
         timeEntries
         |> List.groupBy (_.ProjectName)
         |> List.map (fun (name, _) -> (name, findProjectCode package name))
 
+    let mutable row = 7
+
     for name, code in projectCodes do
-        match code with
-        | None ->
-            prestations.Cells[row, 2].Value <- "AUTRE"
-            prestations.Cells[row, 3].Value <- name
-        | Some code -> prestations.Cells[row, 2].Value <- code
+        prestations.Cells[row, 2].Value <- code
+        prestations.Cells[row, 3].Value <- name
 
         row <- row + 1
 
-    // prestations.Cells["C:C"]
-    // |> getProjectRows
-    // |> Map.iter (fun project row ->
-    //     for col, date in days do
-    //         timeEntries
-    //         |> List.tryFind (fun te -> te.Date = date && te.ProjectName = project)
-    //         |> Option.iter (fun timeEntry -> prestations.Cells[row, col + 4].Value <- timeEntry.Duration))
+    package
+
+let setupDate date (package: ExcelPackage) =
+    package.Workbook.Worksheets["Configuration"].Cells["D13"].Value <- date
+    package
+
+let generateExcel path date timeEntries =
+    let package =
+        new ExcelPackage("Timesheet-Template-v10.xlsx")
+        |> setupDate date
+        |> setupProjects timeEntries
+
+    let prestations = package.Workbook.Worksheets["Prestations"]
+
+    let days = date |> generateDaysOfMonth |> Seq.indexed
+
+    prestations.Cells["C:C"]
+    |> getProjectRows
+    |> Map.iter (fun project row ->
+        for col, date in days do
+            timeEntries
+            |> List.tryFind (fun te -> te.Date = date && te.ProjectName = project)
+            |> Option.iter (fun timeEntry -> prestations.Cells[row, col + 4].Value <- timeEntry.Duration))
 
     let savePath =
         Path.Combine(path, $"TS-{date:yyyyMM}-Delcoigne-Vincent.xlsx") |> FileInfo
