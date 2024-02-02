3f1371f754f6bc5ae4992bac885feb9c846df33f
Vincent DELCOIGNE <vdelcoigne@actiris.be>
2024-02-02 15:47:09 +0100

    Setup the projects present in month

--- TimesheetFs/Excel.fs
+++ TimesheetFs/Excel.fs
@@ -13,14 +13,13 @@ let getProjectRows(range: ExcelRange) =
     |> Seq.map (fun cell -> (cell.Value |> string, cell.Start.Row))
     |> Map.ofSeq
 
-let findProjectCode (package: ExcelPackage) name =
+let findProjectCode (package: ExcelPackage) (name: string) =
     let projectsSheet = package.Workbook.Worksheets["Codes projet"]
     let range = projectsSheet.Cells["B:B"]
 
     range
     |> Seq.tryFind (fun cell -> cell.Value = name)
-    |> Option.map (fun cell -> projectsSheet.Cells[cell.Start.Row, 1].Value)
-    |> Option.defaultValue "AUTRE"
+    |> Option.map (fun cell -> projectsSheet.Cells[cell.Start.Row, 1].Value |> string)
 
 let generateExcel path date timeEntries =
     let package = new ExcelPackage("Timesheet-Template-v10.xlsx")
@@ -30,13 +29,29 @@ let generateExcel path date timeEntries =
 
     let days = date |> generateDaysOfMonth |> Seq.indexed
 
-    prestations.Cells["C:C"]
-    |> getProjectRows
-    |> Map.iter (fun project row ->
-        for col, date in days do
-            timeEntries
-            |> List.tryFind (fun te -> te.Date = date && te.ProjectName = project)
-            |> Option.iter (fun timeEntry -> prestations.Cells[row, col + 4].Value <- timeEntry.Duration))
+    let mutable row = 7
+
+    let projectCodes =
+        timeEntries
+        |> List.groupBy (_.ProjectName)
+        |> List.map (fun (name, _) -> (name, findProjectCode package name))
+
+    for name, code in projectCodes do
+        match code with
+        | None ->
+            prestations.Cells[row, 2].Value <- "AUTRE"
+            prestations.Cells[row, 3].Value <- name
+        | Some code -> prestations.Cells[row, 2].Value <- code
+
+        row <- row + 1
+
+    // prestations.Cells["C:C"]
+    // |> getProjectRows
+    // |> Map.iter (fun project row ->
+    //     for col, date in days do
+    //         timeEntries
+    //         |> List.tryFind (fun te -> te.Date = date && te.ProjectName = project)
+    //         |> Option.iter (fun timeEntry -> prestations.Cells[row, col + 4].Value <- timeEntry.Duration))
 
     let savePath =
         Path.Combine(path, $"TS-{date:yyyyMM}-Delcoigne-Vincent.xlsx") |> FileInfo
