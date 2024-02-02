ca97fb80409b3fe13453cd9ca114a4993d49ddc3
Vincent DELCOIGNE <vdelcoigne@actiris.be>
2024-02-02 14:18:27 +0100

    Find project row

--- TimesheetFs/EPPlusExcel.fs
+++ TimesheetFs/EPPlusExcel.fs
@@ -5,30 +5,35 @@ open System.IO
 open Types
 open Common
 
-// TODO : 
-// find project row
-// utiliser user secret
+// TODO 
+// gérer nom et prénom
+
+let getProjectRows(range: ExcelRange) =
+    [ for cell in range do
+          (cell.Value |> string, cell.Start.Row) ]
+    |> Map.ofList
 
 let generateExcelFromTemplate path date (timeEntries: MyTimeEntry list) =
     let savePath = Path.Combine(path, $"TS-{date:yyyyMM}-Delcoigne-Vincent-WIP.xlsx")
 
-    let dates = date |> generateDates |> Seq.toList
-    let projects = (timeEntries |> List.groupBy (_.ProjectName) |> List.sort)
-    let package = new ExcelPackage(Path.Combine(path, "Timesheet-Template-v10.xlsx"))
-
+    let package = new ExcelPackage("Timesheet-Template-v10.xlsx")
     package.Workbook.Worksheets["Configuration"].Cells["D13"].Value <- date
 
     let prestations = package.Workbook.Worksheets["Prestations"]
 
-    let mutable row = 7
+    let map = prestations.Cells["C:C"] |> getProjectRows
 
-    for _, timeEntries in projects do
-        for col, date in dates |> List.indexed do
+    let dates = date |> generateDates |> Seq.indexed
+    let projects = (timeEntries |> List.groupBy (_.ProjectName) |> List.sort)
+    
+    for projectName, timeEntries in projects do
+        for col, date in dates do
             timeEntries
             |> List.tryFind (fun te -> te.Date = date)
-            |> Option.iter (fun item -> prestations.Cells[row, col + 4].Value <- item.Duration)
-
-        row <- row + 1
+            |> Option.iter (fun item ->
+                match (map |> Map.tryFind projectName) with
+                | None -> ()
+                | Some row -> prestations.Cells[row, col + 4].Value <- item.Duration)
 
     package.SaveAs(FileInfo(savePath))
 
