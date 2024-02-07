2aad6401518f151ed2f437d53ddda3461321f92e
Vincent DELCOIGNE <vdelcoigne@actiris.be>
2024-02-07 14:47:40 +0100

    Add test project for outlook

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Office.Interop.Outlook;
+
+PrepareOutlookMail(DateTime.Today, @"c:\temp\TS-202401-Delcoigne-Vincent.xlsx");
+
+void PrepareOutlookMail(DateTime date, string attachmentPath)
+{
+    var mail = new Application().CreateItem(OlItemType.olMailItem) as MailItem;
+    mail.Subject = $"Timesheet {date:Y}";
+    var recipTo = mail.Recipients.Add("pnijs@actiris.be");
+    recipTo.Type = (int) OlMailRecipientType.olTo;
+    var recipCc = mail.Recipients.Add("schauvaux@actiris.be");
+    recipCc.Type = (int) OlMailRecipientType.olTo;
+
+    mail.Attachments.Add(attachmentPath,
+        OlAttachmentType.olByValue, Type.Missing, Type.Missing);
+
+    mail.Body =
+        $"""
+         Bonjour,
+
+         Voici ma Timesheet pour le mois de {date:Y}.
+
+         Cordialement,
+
+         Delcoigne Vincent.
+         """;
+    mail.Recipients.ResolveAll();
+    mail.Display(false);
+}
